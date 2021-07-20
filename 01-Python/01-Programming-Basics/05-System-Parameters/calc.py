e35ebfcd505bfe7efe679de3a32f5bbdc26aa1d6
Fernando Rivero <nandosoq@gmail.com>
2021-07-20 16:35:19 -0500

    We made it

--- 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
+++ 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
@@ -4,9 +4,10 @@ import sys
 
 def main():
     """Implement the calculator"""
-    pass  # YOUR CODE HERE
+
+    return eval(sys.argv[1] + sys.argv[2] + sys.argv[3])
+    #pass  # YOUR CODE HERE
 
 
 if __name__ == "__main__":
     print(main())
-    
