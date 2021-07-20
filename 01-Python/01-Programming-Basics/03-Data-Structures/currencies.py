768390d33285da36db5a59f8afe3e7d18089bb27
Fernando Rivero <nandosoq@gmail.com>
2021-07-20 15:17:55 -0500

    I like my fx

--- 01-Python/01-Programming-Basics/03-Data-Structures/currencies.py
+++ 01-Python/01-Programming-Basics/03-Data-Structures/currencies.py
@@ -1,11 +1,25 @@
 # pylint: disable=missing-docstring
 
 # TODO: add some currency rates
-RATES = {}
+RATES = {'USDEUR': 0.85,
+'GBPEUR': 1.13,
+'CHFEUR': 0.86,
+'EURGBP': 0.885,
+'MXNUSD': 0.05,
+'USDMXN': 20,
+'CHFUSD': 1.08}
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
     currency is a string
     """
-    pass  # YOUR CODE HERE
+
+    if RATES.get(str(amount[1] + currency) ,0):
+        fx = RATES.get(str(amount[1] + currency) ,0)
+    elif RATES.get(str(currency + amount[1]) ,0):
+        fx = 1 / RATES.get(str(currency + amount[1]) ,0)
+    else:
+        return None
+
+    return round(fx*amount[0])
