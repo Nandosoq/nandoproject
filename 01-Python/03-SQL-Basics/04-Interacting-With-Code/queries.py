8054d2617a1dbf5d45849efe208e199afd833aeb
Fernando Rivero <nandosoq@gmail.com>
2021-07-22 15:18:21 -0500

    review ALWAYS the ID

--- 01-Python/03-SQL-Basics/04-Interacting-With-Code/queries.py
+++ 01-Python/03-SQL-Basics/04-Interacting-With-Code/queries.py
@@ -1,25 +1,41 @@
 # pylint: disable=missing-docstring, C0103
 
 def directors_count(db):
-    # return the number of directors contained in the database
-    pass  # YOUR CODE HERE
-
+    query = 'SELECT count(*) FROM directors d'
+    db.execute(query)
+    rows = db.fetchone()
+    return rows[0]
 
 def directors_list(db):
-    # return the list of all the directors sorted in alphabetical order
-    pass  # YOUR CODE HERE
-
+    query = 'SELECT name FROM directors d ORDER BY name ASC'
+    db.execute(query)
+    rows = db.fetchall()
+    list_direct = []
+    for row in rows:
+        list_direct.append(row[0])
+    return list_direct
 
 def love_movies(db):
-    # return the list of all movies which contain the word "love" in their title, sorted in alphabetical order
-    pass  # YOUR CODE HERE
-
+    query = "SELECT title FROM movies WHERE title LIKE '%love%' ORDER BY title ASC"
+    db.execute(query)
+    rows = db.fetchall()
+    list_love = []
+    for row in rows:
+        list_love.append(row[0])
+    return list_love
 
 def directors_named_like_count(db, name):
-    # return the number of directors which contain a given word in their name
-    pass  # YOUR CODE HERE
-
+    query = "SELECT count(*) from directors d WHERE name LIKE '%" + name + "%'"
+    db.execute(query)
+    rows = db.fetchone()
+    return rows[0]
 
 def movies_longer_than(db, min_length):
-    # return this list of all movies which are longer than a given duration, sorted in the alphabetical order
-    pass  # YOUR CODE HERE
+    query = "SELECT title from movies GROUP BY id HAVING minutes >'" + str(
+        min_length) + "' ORDER BY title ASC"
+    db.execute(query)
+    rows = db.fetchall()
+    list_film_lenght = []
+    for row in rows:
+        list_film_lenght.append(row[0])
+    return list_film_lenght
