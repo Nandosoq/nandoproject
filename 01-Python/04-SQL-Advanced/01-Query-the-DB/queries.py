0368cd3c9193b50af4628910e4cc9156fe22629c
Fernando Rivero <nandosoq@gmail.com>
2021-07-23 12:29:06 -0500

    Julianday()

--- 01-Python/04-SQL-Advanced/01-Query-the-DB/queries.py
+++ 01-Python/04-SQL-Advanced/01-Query-the-DB/queries.py
@@ -1,15 +1,39 @@
 # pylint:disable=C0111,C0103
 
 def query_orders(db):
-    # return a list of orders displaying each column
-    pass  # YOUR CODE HERE
+    query ='''
+        SELECT *
+        FROM  Orders o
+        ORDER BY OrderID ASC
+    '''
+    db.execute(query)
+    return db.fetchall()
+
 
 def get_orders_range(db, date_from, date_to):
+    query = '''
+        SELECT *
+        FROM  Orders o
+        WHERE OrderDate > ? AND OrderDate < ?
+    '''
+    db.execute(query, (date_from, date_to))
+    #db.execute(query)
+    return db.fetchall()
+
     # return a list of orders displaying all columns with OrderDate between date_from and date_to
-    pass  # YOUR CODE HERE
+
 
 def get_waiting_time(db):
+
+    query = '''
+        SELECT *, (julianday(o.ShippedDate) - julianday(o.OrderDate))  AS TimeDelta
+        FROM  Orders o
+        ORDER BY TimeDelta ASC
+    '''
+    db.execute(query)
+    return db.fetchall()
+
     # get a list with all the orders displaying each column
     # and calculate an extra TimeDelta column displaying the number of days
     # between OrderDate and ShippedDate, ordered by ascending TimeDelta
-    pass  # YOUR CODE HERE
+    #pass  # YOUR CODE HERE
