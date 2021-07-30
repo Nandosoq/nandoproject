49143b4a8bdff42211b385f42f7271cbe09e9a22
Fernando Rivero <nandosoq@gmail.com>
2021-07-30 12:05:05 -0500

    going

--- 03-Maths/02-Statistics-Probabilities/01-Mean-Mode-Median-Std/basic_functions.py
+++ 03-Maths/02-Statistics-Probabilities/01-Mean-Mode-Median-Std/basic_functions.py
@@ -1,14 +1,29 @@
 def my_mean(samples):
-    pass
-
+    return sum(samples) / len(samples)
 
 def my_standard_deviation(samples):
-    pass
+    mean = my_mean(samples)
+    a = 0
+    for i in samples:
+        a += (i - mean)**2
+    return (a / (len(samples) - 1))**(1 / 2)
 
 
 def my_median(samples):
-    pass
+    samples.sort()
+
+    if len(samples)%2 ==1:
+        return samples[int(len(samples)/2)]
+    return (samples[int(len(samples)/2)-1] + samples[int(len(samples)/2)])/2
 
 
 def my_mode(samples):
-    pass
+    a=0
+    value=0
+    for i in samples:
+        if samples.count(i)>a:
+            a = samples.count(i)
+            value = i
+            print(a)
+            print(value)
+    return value
