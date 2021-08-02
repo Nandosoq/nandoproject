788affab7afb67a5fb64b8e17a19a980df5ed735
Fernando Rivero <nandosoq@gmail.com>
2021-08-02 14:26:23 -0500

    We are the campions!!

--- 04-Decision-Science/olist/data.py
+++ 04-Decision-Science/olist/data.py
@@ -11,10 +11,42 @@ class Olist:
         Its values should be pandas.DataFrame loaded from csv files
         """
         # Hints: Build csv_path as "absolute path" in order to call this method from anywhere.
+        csv_path = '/home/nandosoq/code/Nandosoq/data-challenges/04-Decision-Science/data/csv'
         # Do not hardcode your path as it only works on your machine ('Users/username/code...')
         # Use __file__ as absolute path anchor independant of your computer
+
         # Make extensive use of `import ipdb; ipdb.set_trace()` to investigate what `__file__` variable is really
         # Use os.path library to construct path independent of Unix vs. Windows specificities
+
+        #Create the list file_names containing all csv file names in the csv directory
+        file_names = os.listdir(csv_path)
+
+        for v in file_names:
+            if v[-3:] != 'csv':
+                file_names.remove(v)
+
+
+        #Create the list of dict key key_names
+        key_names = []
+
+        for v in file_names:
+
+            txt = ''
+            txt = v.replace('_dataset.csv', '')
+            txt = txt.replace(".csv", "")
+            txt = txt.replace('olist_', '')
+
+            key_names.append(txt)
+
+        #Construct the dictionary data
+
+        data = {}
+
+        for (x, y) in zip(key_names, file_names):
+
+            data[x] = pd.read_csv(os.path.join(csv_path, y))
+
+        return data
 
     def get_matching_table(self):
         """
